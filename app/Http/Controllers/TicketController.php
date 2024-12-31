50d7953d09be508685c30d21a54152cc7914ce25
Muzola Ethberg <ethbergmuzola500@gmail.com>
2024-12-31 11:56:37 +0100

    Refactor: Possibiliter de faire des recherches et updater les presences

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,76 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\TicketCollection;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Models\Ticket;
 use App\Models\PresenceHistory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class TicketController extends Controller
 {
-    public function store(Request $request)
-{
-    // Valider les données entrées par l'utilisateur
-    $validatedData = $request->validate([
-        'serie' => 'nullable|string',
-        'name' => 'nullable|string',
-        'price' => 'nullable|numeric',
-        'barcode' => 'nullable|string',
-        'qrcode' => 'nullable|string',
-        'eventName' => 'nullable|string',
-        'eventAddress' => 'nullable|string',
-        'eventDate' => 'nullable|date',
-        'eventEndDate' => 'nullable|date',
-        'eventCurrency' => 'nullable|string|max:3',
-        'eventId' => 'nullable|integer',
-        'status' => 'nullable|string|in:0,1',
-    ]);
-
-    // Créer un nouvel événement
-    $event = Ticket::create($validatedData);
-
-    // Retourner une réponse JSON avec les données de l'événement créé
-    return response()->json([
-        'status'=>200,
-        'message'=>'Reussie',
-        'event'=>$event
-    ],200);
-}
-
-public function scan(Request $request) {
-  }
-  public function scanBest(Request $request) {
-    $validated = $request->validate(['qrcode' => 'required|string']);
-    $qrcode = $validated['qrcode'];
-    $ticket = Ticket::where('qrcode', $qrcode)->first();
-
-    if (!$ticket) {
-        return response()->json(['message' => 'no'], 404);
+    public function getAllTickets()
+    {
+        $tickets = Ticket::all();
+        return new TicketCollection($tickets);
     }
-    $todayFalse=Carbon::create('2024-09-16')->format('Y-m-d');
-    $today = now()->format('Y-m-d');
-    if ($ticket->last_reset_date !== $today) {
-        // Réinitialiser le statut du ticket spécifique
-        $ticket->status = 0;
-        $ticket->last_reset_date = $today;
+    public function searchTickets(Request $request)
+    {
+        $query = $request->input('query');
+
+        $tickets = Ticket::where('nom', 'LIKE', "%{$query}%")
+            ->orWhere('postnom', 'LIKE', "%{$query}%")
+            ->orWhere('prenom', 'LIKE', "%{$query}%")
+            ->orWhere('civilite', 'LIKE', "%{$query}%")
+            ->get();
+
+        return new TicketCollection($tickets);
+    }
+    public function updatePresence(Request $request, $id)
+    {
+        $request->validate([
+            'presence' => 'required|boolean',
+        ]);
+
+        $ticket = Ticket::find($id);
+
+        if (!$ticket) {
+            return response()->json(['error' => 'Ticket not found'], 404);
+        }
+
+        $ticket->presence = $request->input('presence');
         $ticket->save();
+
+        return response()->json(['message' => 'Ok', 'ticket' => $ticket]);
     }
-
-    // Vérifiez si le ticket a déjà été scanné
-    if ($ticket->status == 1) {
-        return response()->json(['message' => 'noscan'], 400);
-    }
-
-    $ticket->status = 1;
-    $ticket->save();
-
-
-    PresenceHistory::create([
-        'TicketId' => $ticket->TicketId,
-        'scan_date' => now(),
-        'status' => 1,
-    ]);
-
-    return response()->json(['message' => 'scan'], 200);
-}
 }
